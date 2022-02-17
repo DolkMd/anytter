4d55eb4045157c24fce0990c437a8eacf96d56c7
yumetanichihiro <fcfcqloows@gmail.com>
2022-02-18 00:43:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import os
+import feedparser
 import dotenv
+import subprocess
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from infra import twitter, rss
@@ -15,10 +17,11 @@ RSS_URL = os.getenv('BLOG_RSS_URL')
 
 scheduler = BlockingScheduler()
 twitter_client = twitter.get_client(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-feed_entry = random.choice(rss.get_feed(RSS_URL).entries)
+feed = rss.get_feed(RSS_URL).entries
 
 @scheduler.scheduled_job('interval', minutes=30)
 def job_30_minutes():
+    feed_entry = random.choice(feed)
     task1_note_tweet.tweet_note(twitter_client.update_status, feed_entry.title, feed_entry.links[0].href)
 
 scheduler.start()
